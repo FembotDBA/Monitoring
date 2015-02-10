175a838ef2e2f81468c05101f8b197431e35de0e
fembotdba <lararasner@gmail.com>
2015-02-09 20:34:56 -0800

    Initial Database

---
+++ Monitoring/dbo/Tables/MonitoredServer.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[MonitoredServer] (
+    [ServerId]   INT            IDENTITY (1, 1) NOT NULL,
+    [ServerName] NVARCHAR (128) NULL,
+    [ConnString] NVARCHAR (250) NULL
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Server Identity Field', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MonitoredServer', @level2type = N'COLUMN', @level2name = N'ServerId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Name of server', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MonitoredServer', @level2type = N'COLUMN', @level2name = N'ServerName';
+
